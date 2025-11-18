a463a673cb43200a0680790daec90a5c58054ae8
p-rint <159627419+p-rint@users.noreply.github.com>
2025-11-18 17:04:09 -0500

    First commit!!!
    Made screen shake, a working camera, basic movement, and learning particles.

---
+++ cam_pivot.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+var mouseLock = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		#print("a")
+		#print(event.relative.x)
+		rotation.y -= event.relative.x * .001 #Left <-> Right
+		rotation.x += event.relative.y * .001 #Up <-> Down
+		
+		rotation.y = wrapf(rotation.y, 0, PI)
+		rotation.x = clampf(rotation.x,-PI/2,PI/2)
+		
+		print(rotation.y)
+		
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if Input.is_action_just_pressed("Escape"):
+		if mouseLock:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		mouseLock = not mouseLock
+	pass
+		
